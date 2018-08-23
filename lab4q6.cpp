05a7f179460d13ed6a28eee860ca4018c5c61e7e
DebdootGhosh <42205626+DebdootGhosh@users.noreply.github.com>
2018-08-23 11:31:56 +0530

    Add files via upload

---
+++ lab4q6.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+float base, height, area;
+cout << " calculate the area of a triangle from base and height :\n";
+cout << " input the value of base : " << endl;
+cin >> base;
+cout << " input the value of height : " << endl;
+cin >> height;
+area = 0.5 *  base * height ;
+cout << " the area of the triagle : " << area << endl;
+return 0;
+}
