3b98e27edbd09c488e78ba5acca60d0bac696824
DebdootGhosh <42205626+DebdootGhosh@users.noreply.github.com>
2018-08-23 10:13:50 +0530

    Add files via upload

---
+++ lab4q2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main() {
+float fahrenheit, celsius;
+cout << " Enter the temperature in celsius : ";
+cin >> celsius;
+fahrenheit = ( celsius * 9.0 ) / 5.0 + 32;
+cout << " The temperature in celsius " << celsius << endl;
+cout << " The temperature in fahrenheit " << fahrenheit << endl;
+return 0;
+}
