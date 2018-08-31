4eacd62f1d6fabfab43d481a92620b7a8ac9528b
DebdootGhosh <42205626+DebdootGhosh@users.noreply.github.com>
2018-08-31 12:15:15 +0530

    Add files via upload

---
+++ lab5q23.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+int n;
+cout << " print all natural numbers in reverse from n to 1  : ";
+cin >> n;
+int i = n;
+while (i >= 1) {
+cout << i << endl;
+i=i-1;
+}
+return 0;
+}
