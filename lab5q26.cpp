1985f6bec6d6db0da32184d3b30c8140a9cf20c0
DebdootGhosh <42205626+DebdootGhosh@users.noreply.github.com>
2018-09-05 16:34:36 +0530

    Add files via upload

---
+++ lab5q26.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+int n;
+cout << "print all odd numbers between 1 to 100 : " ;
+cin >> n;
+int i = 1;
+while ( i <= 100 ) {
+    cout << i << endl;
+    i = i + 2;
+ }
+  return 0;
+ }
