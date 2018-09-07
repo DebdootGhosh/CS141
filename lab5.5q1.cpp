a9940e6631668c6cef412931ef198ae32e50ec8e
DebdootGhosh <42205626+DebdootGhosh@users.noreply.github.com>
2018-09-07 10:42:45 +0530

    Add files via upload

---
+++ lab5.5q1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() { 
+int n;
+cin>>n;
+for ( int i= 0; i< 5;i++ )// print n times the following
+{for (int i=0; i<5; i++)
+{//print n stars
+cout<< " * ";}// go to the next line
+cout<<endl;
+}
+return 0;
+ }
